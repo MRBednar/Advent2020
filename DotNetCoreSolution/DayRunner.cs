1cd5d4520127658456337695f6dc62d70a3531c9
MR Bednar <M.R.Bednar@gmail.com>
2020-12-01 22:42:31 -0800

    Day2 Part 1 finished, added all days to runner

--- DotNetCoreSolution/DayRunner.cs
+++ DotNetCoreSolution/DayRunner.cs
@@ -16,6 +16,30 @@ namespace Advent2020.DotNetCoreSolution
             dayArgument = new Dictionary<int, IDay>
             {
                 {1, new Day1() },
+                {2, new Day2() },
+                {3, new Day3() },
+                {4, new Day4() },
+                {5, new Day5() },
+                {6, new Day6() },
+                {7, new Day7() },
+                {8, new Day8() },
+                {9, new Day9() },
+                {10, new Day10() },
+                {11, new Day11() },
+                {12, new Day12() },
+                {13, new Day13() },
+                {14, new Day14() },
+                {15, new Day15() },
+                {16, new Day16() },
+                {17, new Day17() },
+                {18, new Day18() },
+                {19, new Day19() },
+                {20, new Day20() },
+                {21, new Day21() },
+                {22, new Day22() },
+                {23, new Day23() },
+                {24, new Day24() },
+                {25, new Day25() },
             };
     }
 }
--- DotNetCoreSolution/Days/SolutionDays/Day2.cs
+++ DotNetCoreSolution/Days/SolutionDays/Day2.cs
@@ -12,17 +12,38 @@ namespace Advent2020.DotNetCoreSolution.Days.SolutionDays
 
         public override string Run()
         {
-
-            return string.Format("Not Yet Implemented");
+            var p1Result = Part1().Result;
+            return string.Format("Part1: {0}", p1Result);
         }
 
-        private async Task Part1 ()
+        private async Task<string> Part1 ()
         {
             using (AmazonS3Client s3Client = new AmazonS3Client())
             {
                 var getInput = new GetInputFromS3(s3Client);
-                dayInput = await getInput.GetDayInput(1);
+                dayInput = await getInput.GetDayInput(2);
             }
+
+            var validPWCount = 0;
+            foreach(string inputRow in dayInput)
+            {
+                var policyAndPW = inputRow.Split(':');
+                var potentialPW = policyAndPW.Last();
+                var countAndLetter = policyAndPW.First().Split(' ');
+                var letterCheck = countAndLetter.Last();
+                var letterCount = potentialPW.Count(x => x == letterCheck.FirstOrDefault());
+                var minAndMax = countAndLetter.First().Split('-');
+
+                if (letterCount >= int.Parse(minAndMax.First()))
+                {
+                    if (letterCount <= int.Parse(minAndMax.Last()))
+                    {
+                        validPWCount++;
+                    }
+                }
+            }
+
+            return validPWCount.ToString();
         }
 
         private async Task Part2()
