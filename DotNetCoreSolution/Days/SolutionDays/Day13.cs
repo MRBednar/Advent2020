dc0a7cad851c372eb88405ef3353ea137ad0525e
MR Bednar <M.R.Bednar@gmail.com>
2021-04-21 15:52:55 -0700

    Day 13 Part 1 Solution

--- DotNetCoreSolution/Days/SolutionDays/Day13.cs
+++ DotNetCoreSolution/Days/SolutionDays/Day13.cs
@@ -12,13 +12,36 @@ namespace Advent2020.DotNetCoreSolution.Days.SolutionDays
 
         public override string Run()
         {
-
-            return string.Format("Not Yet Implemented");
+            var p1Return = Part1().Result;
+            return string.Format(p1Return);
         }
 
-        private async Task Part1 ()
+        private async Task<string> Part1 ()
         {
+            using (AmazonS3Client s3Client = new AmazonS3Client())
+            {
+                var getInput = new GetInputFromS3(s3Client);
+                dayInput = await getInput.GetDayInput(13);
+            }
 
+            var earleistDepature = int.Parse(dayInput[0]);
+            var busStringList = dayInput[1].Split(',');
+            var validBusStringList = busStringList.Where(busString => busString != "x");
+            var validBusList = validBusStringList.Select(busString => int.Parse(busString)).ToList();
+            var busWaitTime = new List<KeyValuePair<int, int>>();
+            foreach(var validBus in validBusList)
+            {
+                var remander = earleistDepature % validBus;
+                var departTime = earleistDepature + (validBus - remander);
+                busWaitTime.Add(new KeyValuePair<int,int>(departTime, validBus));
+            }
+
+            var earliestBusList = busWaitTime.OrderBy(x => x.Key).ToList();
+
+            var earliestBus = earliestBusList.FirstOrDefault();
+
+            var returnCode = (earliestBus.Key - earleistDepature) * earliestBus.Value;
+            return string.Format("Earliest Depart Time: {0}, Earliest Bus Number: {1}, Code: {2}", earliestBus.Key, earliestBus.Value, returnCode);
         }
 
         private async Task Part2()
